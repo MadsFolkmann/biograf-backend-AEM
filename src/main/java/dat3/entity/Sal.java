e23f9fe4a01878acff64dbd3d769b31ef88c8e8f
Abdiox <abdirahmanroble2610@gmail.com>
2024-03-12 14:48:51 +0100

    lavet sal klasse om

--- src/main/java/dat3/entity/Sal.java
+++ src/main/java/dat3/entity/Sal.java
@@ -2,21 +2,11 @@ package dat3.entity;
 
 import dat3.enums.SalType;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
-import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
-@Getter
-@Setter
-@NoArgsConstructor
 @Entity
-@Table(name = "sal")
-
 public class Sal {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,9 +16,56 @@ public class Sal {
     private int antalSæderPrRække;
     private SalType salType;
 
-    @CreationTimestamp
-    private LocalDateTime created;
 
-    @UpdateTimestamp
-    private LocalDateTime edited;
+    public Sal() {
+    }
+
+    public Sal(int nummer, int antalRækker, int antalSæderPrRække, SalType salType) {
+        this.nummer = nummer;
+        this.antalRækker = antalRækker;
+        this.antalSæderPrRække = antalSæderPrRække;
+        this.salType = salType;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNummer() {
+        return nummer;
+    }
+
+    public void setNummer(int nummer) {
+        this.nummer = nummer;
+    }
+
+    public int getAntalRækker() {
+        return antalRækker;
+    }
+
+    public void setAntalRækker(int antalRækker) {
+        this.antalRækker = antalRækker;
+    }
+
+    public int getAntalSæderPrRække() {
+        return antalSæderPrRække;
+    }
+
+    public void setAntalSæderPrRække(int antalSæderPrRække) {
+        this.antalSæderPrRække = antalSæderPrRække;
+    }
+
+    public SalType getSalType() {
+        return salType;
+    }
+
+    public void setSalType(SalType salType) {
+        this.salType = salType;
+    }
+
+
 }
