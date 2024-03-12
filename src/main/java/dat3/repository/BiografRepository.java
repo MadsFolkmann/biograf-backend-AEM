4071d2e1e46216436fda6bba05fdfbcacefede8b
Abdiox <abdirahmanroble2610@gmail.com>
2024-03-12 14:52:18 +0100

    fjernet film entity import

--- src/main/java/dat3/repository/BiografRepository.java
+++ src/main/java/dat3/repository/BiografRepository.java
@@ -1,10 +1,7 @@
 package dat3.repository;
 
 import dat3.entity.Biograf;
-import dat3.entity.Film;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
 
 public interface BiografRepository extends JpaRepository<Biograf, Integer>{
 }
