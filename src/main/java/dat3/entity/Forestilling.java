bc104e237ab6fa8dd535a9965662e9335d71a12c
Abdiox <abdirahmanroble2610@gmail.com>
2024-03-12 14:42:49 +0100

    lavet om

--- src/main/java/dat3/entity/Forestilling.java
+++ src/main/java/dat3/entity/Forestilling.java
@@ -1,18 +1,11 @@
 package dat3.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.Set;
 
-@Getter
-@Setter
-@NoArgsConstructor
 @Entity
-@Table(name = "forestilling")
 public class Forestilling {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,4 +20,62 @@ public class Forestilling {
     private Set<Sæde> sæder;
 
     private LocalDateTime tidspunkt;
+
+    public Forestilling() {
+    }
+
+    public Forestilling(Biograf biograf, Film film, Sal sal, LocalDateTime tidspunkt) {
+        this.biograf = biograf;
+        this.film = film;
+        this.sal = sal;
+        this.tidspunkt = tidspunkt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Biograf getBiograf() {
+        return biograf;
+    }
+
+    public void setBiograf(Biograf biograf) {
+        this.biograf = biograf;
+    }
+
+    public Film getFilm() {
+        return film;
+    }
+
+    public void setFilm(Film film) {
+        this.film = film;
+    }
+
+    public Sal getSal() {
+        return sal;
+    }
+
+    public void setSal(Sal sal) {
+        this.sal = sal;
+    }
+
+    public LocalDateTime getTidspunkt() {
+        return tidspunkt;
+    }
+
+    public void setTidspunkt(LocalDateTime tidspunkt) {
+        this.tidspunkt = tidspunkt;
+    }
+
+    public Set<Sæde> getSæder() {
+        return sæder;
+    }
+
+    public void setSæder(Set<Sæde> sæder) {
+        this.sæder = sæder;
+    }
 }
