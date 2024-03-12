71292a594bb2c88cd8fff3daaaaec0274424112e
Abdiox <abdirahmanroble2610@gmail.com>
2024-03-12 14:49:19 +0100

    lavet sæde klasse om

--- src/main/java/dat3/entity/Sæde.java
+++ src/main/java/dat3/entity/Sæde.java
@@ -1,16 +1,12 @@
 package dat3.entity;
 
 import dat3.enums.SædeType;
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-@Getter
-@Setter
-@NoArgsConstructor
 @Entity
-@Table(name = "sæde")
 public class Sæde {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,4 +16,63 @@ public class Sæde {
     private SædeType sædeType;
     private double pris;
     private boolean optaget;
+
+    public Sæde() {
+    }
+
+    public Sæde(int række, int sædeNummer, SædeType sædeType, double pris, boolean optaget) {
+        this.række = række;
+        this.sædeNummer = sædeNummer;
+        this.sædeType = sædeType;
+        this.pris = pris;
+        this.optaget = optaget;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getRække() {
+        return række;
+    }
+
+    public void setRække(int række) {
+        this.række = række;
+    }
+
+    public int getSædeNummer() {
+        return sædeNummer;
+    }
+
+    public void setSædeNummer(int sædeNummer) {
+        this.sædeNummer = sædeNummer;
+    }
+
+    public SædeType getSædeType() {
+        return sædeType;
+    }
+
+    public void setSædeType(SædeType sædeType) {
+        this.sædeType = sædeType;
+    }
+
+    public double getPris() {
+        return pris;
+    }
+
+    public void setPris(double pris) {
+        this.pris = pris;
+    }
+
+    public boolean isOptaget() {
+        return optaget;
+    }
+
+    public void setOptaget(boolean optaget) {
+        this.optaget = optaget;
+    }
 }
