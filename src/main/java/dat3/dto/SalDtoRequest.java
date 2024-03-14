b61d362e545633417239aef4246cdbf324fb1777
Abdiox <abdirahmanroble2610@gmail.com>
2024-03-14 12:20:16 +0100

    sidste dto

--- src/main/java/dat3/dto/SalDtoRequest.java
+++ src/main/java/dat3/dto/SalDtoRequest.java
@@ -1,41 +1,31 @@
 package dat3.dto;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import dat3.entity.Sal;
 import dat3.enums.SalType;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.List;
-
 @Getter
 @Setter
 @NoArgsConstructor
-@JsonInclude(JsonInclude.Include.NON_NULL)
-public class SalDto {
+@AllArgsConstructor
+public class SalDtoRequest {
+
     private Integer id;
     private Integer nummer;
     private Integer antalRækker;
     private Integer antalSæderPrRække;
     private SalType salType;
 
-    public SalDto(Sal sal) {
+    public SalDtoRequest(Sal sal) {
         this.id = sal.getId();
         this.nummer = sal.getNummer();
         this.antalRækker = sal.getAntalRækker();
         this.antalSæderPrRække = sal.getAntalSæderPrRække();
         this.salType = sal.getSalType();
 
-    }
 
-    public Sal toEntity() {
-        Sal sal = new Sal();
-        sal.setId(this.id);
-        sal.setNummer(this.nummer);
-        sal.setAntalRækker(this.antalRækker);
-        sal.setAntalSæderPrRække(this.antalSæderPrRække);
-        sal.setSalType(this.salType);
-        return sal;
     }
 }
