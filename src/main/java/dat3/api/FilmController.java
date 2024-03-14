beee68c4d6e71569abf6489d837c35052cac1a8b
Abdiox <abdirahmanroble2610@gmail.com>
2024-03-14 12:29:18 +0100

    ændret film controller

--- src/main/java/dat3/api/FilmController.java
+++ src/main/java/dat3/api/FilmController.java
@@ -1,6 +1,7 @@
 package dat3.api;
 
-import dat3.dto.FilmDto;
+import dat3.dto.FilmDtoRequest;
+import dat3.dto.FilmDtoResponse;
 import dat3.service.FilmService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,34 +11,35 @@ import java.util.List;
 @RestController
 @RequestMapping("/film")
 public class FilmController {
-    private FilmService filmService;
+
+    private final FilmService filmService;
 
     public FilmController(FilmService filmService) {
         this.filmService = filmService;
     }
+
     @GetMapping
-    public List<FilmDto> getAllFilms() {
+    public List<FilmDtoResponse> getAllFilms() {
         return filmService.getAllFilms();
     }
 
-    @GetMapping(path ="/{id}")
-    public FilmDto getRecipeById(@PathVariable int id) {
+    @GetMapping(path = "/{id}")
+    public FilmDtoResponse getFilmById(@PathVariable int id) {
         return filmService.getFilmById(id);
     }
 
     @PostMapping
-    public FilmDto addFilm(@RequestBody FilmDto request) {
+    public FilmDtoResponse addFilm(@RequestBody FilmDtoRequest request) {
         return filmService.addFilm(request);
     }
 
     @PutMapping(path = "/{id}")
-    public FilmDto editFilm(@RequestBody FilmDto request,@PathVariable int id) {
-        return filmService.editFilm(request,id);
+    public FilmDtoResponse editFilm(@RequestBody FilmDtoRequest request, @PathVariable int id) {
+        return filmService.editFilm(request, id);
     }
 
     @DeleteMapping(path = "/{id}")
-    public ResponseEntity deleteFilm(@PathVariable int id) {
+    public ResponseEntity<Void> deleteFilm(@PathVariable int id) {
         return filmService.deleteFilm(id);
     }
-
 }
